3df419dd350a29f6519fe06395726a0354f25e9c
Jordi de Kock <jordidekock@gmail.com>
2022-12-01 13:38:40 +0100

    day 1 - fix count top three elves

--- day1/day1.playground/Contents.swift
+++ day1/day1.playground/Contents.swift
@@ -10,7 +10,7 @@ elves = content.split(separator: "\n\n")
                .map({ substring in
                     return substring.split(separator: "\n") //Split each set into seperate values
                                     .compactMap{ Int($0) } // Map to Int value
-                                    .reduce(0) { $0 + $1 } // Sum values
+                                    .reduce(0, +) // Sum values
                })
                .sorted { $0 > $1 } // Sort desc.
 
@@ -18,4 +18,4 @@ elves = content.split(separator: "\n\n")
 print(elves.first)
 
 // Top three elves
-print(elves[0] + elves[1] + elves[2])
+print(elves[...2].reduce(0, +))
