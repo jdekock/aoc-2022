d91d586f9b231bead6a045dc85eabc9a2c641812
Jordi de Kock <jordidekock@gmail.com>
2022-12-01 12:49:34 +0100

    Day 1

---
+++ day1/day1.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+import Foundation
+
+let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+var elves: [Int] = []
+
+// Split content into sets for each elf
+elves = content.split(separator: "\n\n")
+               .map({ substring in
+                    let sum = substring.split(separator: "\n") //Split each set into seperate values
+                                       .compactMap{ Int($0) } // Map to Int value
+                                       .reduce(0) { $0 + $1 } // Sum values
+                    return sum
+               })
+
+elves = elves.sorted { $0 > $1 } // Sort desc.
+
+// Most calories
+print(elves.first)
+
+// Top three elves
+print(elves[0] + elves[1] + elves[2])
