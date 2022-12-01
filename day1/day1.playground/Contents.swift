98565ba79ab84215815982b75487b6d99e70971d
Jordi de Kock <jordidekock@gmail.com>
2022-12-01 13:18:55 +0100

    day 1 - return map directly

--- day1/day1.playground/Contents.swift
+++ day1/day1.playground/Contents.swift
@@ -8,10 +8,9 @@ var elves: [Int] = []
 // Split content into sets for each elf
 elves = content.split(separator: "\n\n")
                .map({ substring in
-                    let sum = substring.split(separator: "\n") //Split each set into seperate values
-                                       .compactMap{ Int($0) } // Map to Int value
-                                       .reduce(0) { $0 + $1 } // Sum values
-                    return sum
+                    return substring.split(separator: "\n") //Split each set into seperate values
+                                    .compactMap{ Int($0) } // Map to Int value
+                                    .reduce(0) { $0 + $1 } // Sum values
                })
                .sorted { $0 > $1 } // Sort desc.
 
