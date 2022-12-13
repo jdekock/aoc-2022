edd6779b8bb87b63e16c6a1e170a7b4191dae8da
Jordi de Kock <jordidekock@gmail.com>
2022-12-13 22:15:00 +0100

    day 12 - cleanup and refactor

--- day12/day12.playground/Contents.swift
+++ day12/day12.playground/Contents.swift
@@ -15,10 +15,6 @@ struct Position {
     var height: Int
     var letter: Character
     var path: [[Int]]
-    
-    var description: String {
-        return "\(x),\(y), height: \(height)"
-    }
 }
 
 var points: [[Position]] = [[]]
@@ -48,32 +44,29 @@ for (verticalIndex, line) in data.enumerated() {
 
 points.removeLast()
 
-breadthFirstSearch(from: startPoint!, to: endPoint!, map: points, part2: true)
+// part 1
+breadthFirstSearch(from: endPoint!, to: startPoint, map: points)
 
-func breadthFirstSearch(from: Position, to: Position, map: [[Position]], part2: Bool) {
+// part 2
+breadthFirstSearch(from: endPoint!, to: nil, map: points)
+
+func breadthFirstSearch(from: Position, to: Position?, map: [[Position]]) {
     var map = map
     var queue: [Position] = []
     var visited: Set<[Int]> = Set()
 
-    if part2 {
-        queue.append(to)
-        visited.insert([to.x, to.y])
-    } else {
-        queue.append(from)
-        visited.insert([from.x, from.y])
-    }
+    queue.append(from)
+    visited.insert([from.x, from.y])
 
     while !queue.isEmpty {
         let position = queue.removeFirst()
 
-        if !part2, position.x == to.x && position.y == to.y {
-            print("Found the path!")
-            print(position.path)
+        if let to = to, position.x == to.x && position.y == to.y {
+            print("Found shortest path to top with stepcount:")
             print(position.path.count)
             break
-        } else if position.letter ==  Character("a") {
-            print("Found the path!")
-            print(position.path)
+        } else if to == nil, position.letter ==  Character("a") {
+            print("Found shortest path to a with stepcount:")
             print(position.path.count)
             break
         }
@@ -101,11 +94,7 @@ func breadthFirstSearch(from: Position, to: Position, map: [[Position]], part2: 
             neighbours.append(map[position.y][position.x + 1])
         }
 
-        if !part2 {
-            neighbours = neighbours.filter({ $0.height <= position.height + 1 })
-        } else {
-            neighbours = neighbours.filter({ $0.height >= position.height - 1 })
-        }
+        neighbours = neighbours.filter({ $0.height >= position.height - 1 })
         neighbours.forEach { neighbour in
             if visited.insert([neighbour.x, neighbour.y]).inserted {
                 var neighbour = neighbour
@@ -115,6 +104,4 @@ func breadthFirstSearch(from: Position, to: Position, map: [[Position]], part2: 
             }
         }
     }
-
-    print("Queue empty! No path found.")
 }
---
+++ day13/day13.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+let fileURL = Bundle.main.url(forResource: "input", withExtension: "txt")
+let content = try String(contentsOf: fileURL!, encoding: String.Encoding.utf8)
+
+let data = content.split(separator: "\n\n")
